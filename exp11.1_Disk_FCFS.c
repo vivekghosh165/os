bac4d1b21bf03b412de28f810521bcd547918498
Vivek Ghosh <vivekghosh165@gmail.com>
2023-05-03 02:32:37 +0530

    Update exp11.1_Disk_FCFS.c

--- exp11.1_Disk_FCFS.c
+++ exp11.1_Disk_FCFS.c
@@ -2,20 +2,28 @@
 #include<stdlib.h>
 int main()
 {
- int ReadyQueue[100],i,n,TotalHeadMov=0,initial;
- //Enter the number of requests
- scanf("%d",&n);
- for(i=0;i<n;i++){
- //Enter the sequence of request
- scanf("%d",&ReadyQueue[i]);
- }
- // Enter initial head position
- scanf("%d",&initial);
- for(i=0;i<n;i++)
- {
- TotalHeadMov=TotalHeadMov+abs(ReadyQueue[i]-
-initial);
- initial=ReadyQueue[i];
- }
- printf("Total Head Movement=%d\n",TotalHeadMov);
+    int RQ[100],i,n,TotalHeadMoment=0,initial;
+    printf("Enter the number of Requests\n");
+    scanf("%d",&n);
+    printf("Enter the Requests sequence\n");
+    for(i=0;i<n;i++)
+     scanf("%d",&RQ[i]);
+    printf("Enter initial head position\n");
+    scanf("%d",&initial);
+    for(i=0;i<n;i++)
+    {
+        TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+        initial=RQ[i];
+    }
+    
+    printf("Total head moment is %d",TotalHeadMoment);
+    return 0;
+    
 }
+/*Enter the number of Request
+8
+Enter the Requests Sequence
+95 180 34 119 11 123 62 64
+Enter initial head position
+50
+Total head movement is 644*/
