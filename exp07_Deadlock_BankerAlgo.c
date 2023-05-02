50fb36e7f5cc999c767c2f690f068155f1d01049
Vivek Ghosh <vivekghosh165@gmail.com>
2023-05-03 02:54:23 +0530

    Update exp07_Deadlock_BankerAlgo.c

--- exp07_Deadlock_BankerAlgo.c
+++ exp07_Deadlock_BankerAlgo.c
@@ -1,5 +1,5 @@
+//exp07_Deadlock_BankerAlgo.c
 #include <stdio.h>
-
 int main()
 {
     int n, m, i, j, k;
@@ -7,40 +7,33 @@ int main()
     scanf("%d", &n);
     printf("Enter the number of resources: ");
     scanf("%d", &m);
-
     int alloc[n][m], max[n][m], avail[m];
-
     printf("Enter the allocation matrix:\n");
     for (i = 0; i < n; i++) {
         for (j = 0; j < m; j++) {
             scanf("%d", &alloc[i][j]);
         }
     }
-
     printf("Enter the maximum need matrix:\n");
     for (i = 0; i < n; i++) {
         for (j = 0; j < m; j++) {
             scanf("%d", &max[i][j]);
         }
     }
-
     printf("Enter the available resources:\n");
     for (i = 0; i < m; i++) {
         scanf("%d", &avail[i]);
     }
-
     int f[n], ans[n], ind = 0;
     for (k = 0; k < n; k++) {
         f[k] = 0;
     }
-
     int need[n][m];
     for (i = 0; i < n; i++) {
         for (j = 0; j < m; j++) {
             need[i][j] = max[i][j] - alloc[i][j];
         }
     }
-
     int y = 0;
     for (k = 0; k < n; k++) {
         for (i = 0; i < n; i++) {
@@ -62,7 +55,6 @@ int main()
             }
         }
     }
-
     int flag = 1;
     for (i = 0; i < n; i++) {
         if (f[i] == 0) {
@@ -71,7 +63,6 @@ int main()
             break;
         }
     }
-
     if (flag == 1) {
         printf("Following is the SAFE Sequence\n");
         for (i = 0; i < n - 1; i++) {
