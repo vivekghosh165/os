fcc4cb8a341f8fd8d5032ff698030b7f03330c16
Vivek Ghosh <vivekghosh165@gmail.com>
2023-05-03 02:57:36 +0530

    Update exp09.2_PageRe_LRU.c

--- exp09.2_PageRe_LRU.c
+++ exp09.2_PageRe_LRU.c
@@ -1,3 +1,4 @@
+//exp09.2_PageRe_LRU.c
 #include<stdio.h>
 void main()
 {
@@ -15,59 +16,59 @@ c++;
 k++;
 for(i=1;i<n;i++)
             {
-                        c1=0;
-                        for(j=0;j<f;j++)
-                        {
-                                    if(p[i]!=q[j])
-                                    c1++;
-                        }
-                        if(c1==f)
-                        {
-                                    c++;
-                                    if(k<f)
-                                    {
-                                                q[k]=p[i];
-k++;
+ c1=0;
+   for(j=0;j<f;j++)
+ {
+      if(p[i]!=q[j])
+     c1++;
+   }
+  if(c1==f)
+       {
+         c++;
+         if(k<f)
+      {
+        q[k]=p[i];
+    k++;
 for(j=0;j<k;j++)
 printf("\t%d",q[j]);
 printf("\n");
-                                    }
-                                    else
-                                    {
-                                                for(r=0;r<f;r++)
+          }
+   else
+     {
+   for(r=0;r<f;r++)
 {
-                                                            c2[r]=0;
+       c2[r]=0;
 for(j=i-1;j<n;j--)
 {
 if(q[r]!=p[j])
 c2[r]++;
 else
 break;
-                                                }
-                                    }
-                                    for(r=0;r<f;r++)
-                                     b[r]=c2[r];
-                                    for(r=0;r<f;r++)
-                                    {
-                                                for(j=r;j<f;j++)
+}
+  }
+   for(r=0;r<f;r++)
+    b[r]=c2[r];
+    for(r=0;r<f;r++)
+ {
+ for(j=r;j<f;j++)
 {
-                                                            if(b[r]<b[j])
+ if(b[r]<b[j])
 {
-                                                                        t=b[r];
+  t=b[r];
 b[r]=b[j];
 b[j]=t;
-                                                            }
-                                                }
-                                    }
-                                    for(r=0;r<f;r++)
-                                    {
-                                                if(c2[r]==b[0])
+  }
+ }
+ }
+ for(r=0;r<f;r++)
+ {
+  if(c2[r]==b[0])
 q[r]=p[i];
 printf("\t%d",q[r]);
-                                    }
-                                    printf("\n");
-                        }
-            }
+ }
+ printf("\n");
+  }
+ }
 }
 printf("\nThe no of page faults is %d",c);
 }
