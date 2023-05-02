24392867d912e1209026f8330e975647c3eaab82
Vivek Ghosh <vivekghosh165@gmail.com>
2023-05-03 02:53:15 +0530

    Update exp05.2_Priority.c

--- exp05.2_Priority.c
+++ exp05.2_Priority.c
@@ -1,11 +1,10 @@
+//exp05.2_Priority.c
 #include<stdio.h> 
-  
 int main() 
 { 
     int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat; 
     printf("Enter Total Number of Process:"); 
-    scanf("%d",&n); 
-  
+    scanf("%d",&n);  
     printf("\nEnter Burst Time and Priority\n"); 
     for(i=0;i<n;i++) 
     { 
@@ -16,7 +15,6 @@ int main()
         scanf("%d",&pr[i]); 
         p[i]=i+1;         
     } 
-  
     for(i=0;i<n;i++) 
     { 
         pos=i; 
@@ -25,46 +23,35 @@ int main()
             if(pr[j]<pr[pos]) 
                 pos=j; 
         } 
-  
         temp=pr[i]; 
         pr[i]=pr[pos]; 
         pr[pos]=temp; 
-  
         temp=bt[i]; 
         bt[i]=bt[pos]; 
-        bt[pos]=temp; 
-  
+        bt[pos]=temp;  
         temp=p[i]; 
         p[i]=p[pos]; 
         p[pos]=temp; 
-    } 
-  
-    wt[0]=0; 
-  
+    }   
+    wt[0]=0;   
     for(i=1;i<n;i++) 
     { 
         wt[i]=0; 
         for(j=0;j<i;j++) 
             wt[i]+=bt[j]; 
-  
-        total+=wt[i]; 
+       total+=wt[i]; 
     } 
-  
     avg_wt=total/n;   
-    total=0; 
-  
+    total=0;  
     printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time"); 
     for(i=0;i<n;i++) 
     { 
         tat[i]=bt[i]+wt[i]; 
         total+=tat[i]; 
         printf("\nP[%d]\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tat[i]); 
-    } 
-  
+    }   
     avg_tat=total/n;  
     printf("\n\nAverage Waiting Time=%d",avg_wt); 
-    printf("\nAverage Turnaround Time=%d\n",avg_tat); 
-  
+    printf("\nAverage Turnaround Time=%d\n",avg_tat);   
 return 0; 
 } 
-
