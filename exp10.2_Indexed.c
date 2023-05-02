952791fb72ec1cda2ed313ef4622d2b51e5b1d8d
Vivek Ghosh <vivekghosh165@gmail.com>
2023-05-03 02:24:24 +0530

    Update exp10.2_Indexed.c

--- exp10.2_Indexed.c
+++ exp10.2_Indexed.c
@@ -25,3 +25,18 @@ void main()
  for(j=0;j<m[i];j++)
  printf("%3d",b[i][j]);
 } 
+/*
+Enter no. of files:2
+ Enter starting block and size of file1: 2 5
+Enter blocks occupied by file1:10
+enter blocks of file1:3
+2 5 4 6 7 2 6 4 7
+Enter starting block and size of file2: 3 4
+Enter blocks occupied by file2:5
+enter blocks of file2: 2 3 4 5 6
+File index length
+ 1 2 10
+2 3 5
+Enter file name: venkat
+file name is:12803
+Index is:0Block occupied are: */
