d202688f5376043ed7a06b9781d267565c531978
Vivek Ghosh <vivekghosh165@gmail.com>
2023-05-03 02:51:47 +0530

    Update exp04.2_SJF.c

--- exp04.2_SJF.c
+++ exp04.2_SJF.c
@@ -1,21 +1,14 @@
+//exp04.2_SJF.c
 #include<stdio.h>
-
 void bub_sort(int arr[], int n)
 {
 int i,j,temp; for(i=0;i<n-1;i++)
 {
 for(j=0;j<n-i-1;j++)
-{
-if (arr[j]>arr[j+1]){ temp=arr[j]; arr[j]=arr[j+1]; arr[j+1]=temp;
-}
-}
-}
-}
-
+{if (arr[j]>arr[j+1]){ temp=arr[j]; arr[j]=arr[j+1]; arr[j+1]=temp;}}}}
 int main()
 {
 int i,j,bt[10],wt[10],tat[10],n; int awt=0,atat=0;
-
 printf("Enter the no. of processes : \n"); scanf("%d",&n);
 printf("Enter the burst time/process time : \n"); for(i=0;i<n;i++)
 {
@@ -33,8 +26,6 @@ tat[i]=bt[i]+wt[i]; awt=awt+wt[i]; atat=atat+tat[i]; bub_sort(bt,n);
 printf("%d \t\t %d\t\t %d\t\t %d\n",i+1,bt[i],wt[i],tat[i]);
 }
 awt=awt/n;
-
 printf("Avg. Waiting Time: %d\n",awt); atat=atat/n;
 printf("Avg. Turn Around Time: %d \n",atat);
 }
-
